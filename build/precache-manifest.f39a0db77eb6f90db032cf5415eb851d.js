e0bb0950a1bf092e2f49a739c0c8f47f918ce1bf
Ivan <vanya-99@list.ru>
2020-04-29 15:24:27 +0300

    bulid

---
+++ build/precache-manifest.f39a0db77eb6f90db032cf5415eb851d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a46d661cae80002e73c6",
+    "url": "/static/css/main.fbc1dcbf.chunk.css"
+  },
+  {
+    "revision": "a46d661cae80002e73c6",
+    "url": "/static/js/main.a46d661c.chunk.js"
+  },
+  {
+    "revision": "afeb21a032b82bbeb7ab",
+    "url": "/static/css/1.f3c9936e.chunk.css"
+  },
+  {
+    "revision": "afeb21a032b82bbeb7ab",
+    "url": "/static/js/1.afeb21a0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d54ab4c961763ebb025f203223b00257",
+    "url": "/index.html"
+  }
+];
